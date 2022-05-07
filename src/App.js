5633d3834ce7654e160b49267f672cbc5f833759
Ahsan Habib <ahsanpub16b@gmail.com>
2022-05-08 03:42:35 +0600

    Home and banner section connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
- 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
