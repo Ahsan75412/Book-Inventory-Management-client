8b2996771ed753ce8b9fe4083277b794f03b0688
Ahsan Habib <ahsanpub16b@gmail.com>
2022-05-12 09:25:50 +0600

    Manage product done here

--- src/Pages/ManageItem/ManageItem.js
+++ src/Pages/ManageItem/ManageItem.js
@@ -1,14 +1,34 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useProducts from '../../hooks/useProduts';
 import Product from '../Home/Product/Product';
-import Inventory from '../Inventory/Inventory';
+import './ManageItem.css';
 
-const ManageItem = ({children}) => {
+const ManageItem = ({ children }) => {
     const [product, setProduct] = useProducts();
 
     const navigate = useNavigate();
+
+
+    // Delete Product code 
+    const handleDelete = (id) => {
+        const proceed = window.confirm('Are you sure you want to delete this product?');
+        if (proceed) {
+            const url = `http://localhost:5000/product/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remainingProducts = product.filter(product => product._id !== id);
+                    setProduct(remainingProducts);
+
+                })
+
+        }
+    }
 
     useEffect(() => {
         fetch('http://localhost:5000/product')
@@ -17,19 +37,19 @@ const ManageItem = ({children}) => {
     })
     return (
         <div>
-              <h2 className='text-white py-5 text-center '>All <span className='color'>Product</span></h2>
+            <h2 className='text-white py-5 text-center '>Manage <span className='color'>Product</span></h2>
             <div className='container pro-items '>
 
                 {
                     product.map(product => <Product
                         key={product._id}
                         Product={product}
-                    ><button className='btn text-white'> Delete<i class="fa-solid fa-trash text-danger px-1"></i></button></Product>)
-                    
+                    ><button className='delete' onClick={() => handleDelete(product._id)}> <i class="fa-solid fa-trash"></i></button></Product>)
+
                 }
-            
+
             </div>
-           
+
         </div>
     );
 };
--- src/hooks/useProduts.js
+++ src/hooks/useProduts.js
@@ -4,7 +4,7 @@ const useProducts = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('localhost:5000/product')
+        fetch('http://localhost:5000/product')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
